c1981668926bb96f35dd4681d54d65eb523869b9
josephc.eth <josephc.eth@gmail.com>
2023-05-12 09:59:00 -0400

    reading post body

--- src/functions/createExcel.ts
+++ src/functions/createExcel.ts
@@ -1,15 +1,43 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext
+} from "@azure/functions";
 
-export async function createExcel(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+import * as ExcelJS from "exceljs";
+export async function createExcel(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  const chunks = [];
+  for await (const chunk of request.body) {
+    chunks.push(chunk);
+  }
+  const requestBody = Buffer.concat(chunks).toString();
+  const data = JSON.parse(requestBody).data;
+  const columns = JSON.parse(requestBody).columns;
+  console.log(`Data: ${JSON.stringify(data)}`);
+  console.log(`Columns: ${JSON.stringify(columns)}`);
 
-    return { body: `Hello, ${name}!` };
-};
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("Sheet1");
+  const buffer = await workbook.xlsx.writeBuffer();
+  
+  return {
+    status: 200,
+    headers: {
+      "Content-Type":
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "Content-Disposition": "attachment; filename=Countries_List.xlsx",
+    },
+    body: buffer,
+  };
+}
 
-app.http('createExcel', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: createExcel
+app.http("createExcel", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: createExcel,
 });
