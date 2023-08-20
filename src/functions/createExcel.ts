2c9ce5163f276268e9a3b96f83485ffc74fdff3b
josephc.eth <josephc.eth@gmail.com>
2023-08-20 07:05:43 -0400

    test

--- src/functions/createExcel.ts
+++ src/functions/createExcel.ts
@@ -2,37 +2,20 @@ import {
   app,
   HttpRequest,
   HttpResponseInit,
-  InvocationContext
+  InvocationContext,
 } from "@azure/functions";
 
-import * as ExcelJS from "exceljs";
 export async function createExcel(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
 
-  const chunks = [];
-  for await (const chunk of request.body) {
-    chunks.push(chunk);
-  }
-  const requestBody = Buffer.concat(chunks).toString();
-  const data = JSON.parse(requestBody).data;
-  const columns = JSON.parse(requestBody).columns;
-  console.log(`Data: ${JSON.stringify(data)}`);
-  console.log(`Columns: ${JSON.stringify(columns)}`);
-
-  const workbook = new ExcelJS.Workbook();
-  const worksheet = workbook.addWorksheet("Sheet1");
-  const buffer = await workbook.xlsx.writeBuffer();
-  
   return {
     status: 200,
-    headers: {
-      "Content-Type":
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      "Content-Disposition": "attachment; filename=Countries_List.xlsx",
+    body: {
+      message: "Hello from createExcel",
     },
-    body: buffer,
   };
 }
 
